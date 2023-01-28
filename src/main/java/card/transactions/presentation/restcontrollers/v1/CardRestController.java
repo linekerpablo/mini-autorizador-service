482f237aa431b95b75447ba9e2e65822cf6082f3
linekerpablo <linekerpablo@gmail.com>
2023-01-28 13:42:15 -0300

    Versao do java alterada para testar deploy

--- src/main/java/card/transactions/presentation/restcontrollers/v1/CardRestController.java
+++ src/main/java/card/transactions/presentation/restcontrollers/v1/CardRestController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
