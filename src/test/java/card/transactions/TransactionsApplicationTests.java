67ce92313ab416d3c6ca3fa16144e95721db4865
linekerpablo <linekerpablo@gmail.com>
2023-01-28 13:04:18 -0300

    Initial

---
+++ src/test/java/card/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package card.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
